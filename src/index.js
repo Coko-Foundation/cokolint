2608ed7a274264d680fcc775d43846ff7d3d256a
Yannis Barlas <yannisbarlas@gmail.com>
2020-10-24 13:15:02 +0300

    feat(stylelint): add stylelint dependencies and first config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const commitizenConfig = require('./commitizen')
 const eslintConfig = require('./eslint')
 const prettierConfig = require('./prettier')
+const stylelintConfig = require('./stylelint')
 
 module.exports = {
   commitizen: commitizenConfig,
   eslint: eslintConfig,
   prettier: prettierConfig,
+  stylelint: stylelintConfig,
 }
