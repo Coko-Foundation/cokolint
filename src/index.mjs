3911856673e137136e1754563259c254f158a20d
Yannis Barlas <yannisbarlas@gmail.com>
2026-01-19 17:05:38 +0200

    fix(*): include file extension for commitizen import

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,6 @@
-import commitizen from './commitizen'
+/* eslint-disable-next-line import/extensions */
+import commitizen from './commitizen.js'
+
 import { defineEslintConfig, serverEslintConfig } from './eslint.mjs'
 import prettier from './prettier.mjs'
 import stylelint from './stylelint.mjs'
